ed72ae99c5c025915d23aa99a2f67d1525bfac5a
Michal Martinek <MichalMartinek@users.noreply.github.com>
2024-03-06 19:45:51 +0100

    make name better

--- app/page.tsx
+++ app/page.tsx
@@ -55,20 +55,11 @@ export default function Page() {
         </div>
         <div className="flex space-x-3">
           <Link
-            href="/protected"
+            href="/translations"
             className="text-stone-400 underline hover:text-stone-200 transition-all"
           >
             Protected Page
           </Link>
-          <p className="text-white">·</p>
-          <a
-            href="https://vercel.com/templates/next.js/prisma-postgres-auth-starter"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-stone-400 underline hover:text-stone-200 transition-all"
-          >
-            Deploy to Vercel
-          </a>
         </div>
       </div>
     </div>
