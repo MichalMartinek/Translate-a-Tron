2fb700f9bd643e206c4b3f66f6340adbc8484086
Michal Martinek <MichalMartinek@users.noreply.github.com>
2024-03-14 21:53:35 +0100

    secret

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -5,6 +5,7 @@ export const authConfig = {
   pages: {
     signIn: "/login",
   },
+  secret: process.env.AUTH_SECRET,
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
